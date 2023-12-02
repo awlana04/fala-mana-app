f6048d1f6f4635e031c5fe79f0cac17528a521f9
Awlana Costa <long0413@pop-os.localdomain>
2023-12-02 11:55:42 -0300

    swipe slides with framer-motion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,22 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-import Logo from '../../public/logo.svg';
+import Logo from '../../public/Logo.svg';
 
 export default function Home() {
   return (
-    <body className="grid-flow-col transition">
-      <Image
-        src={Logo}
-        alt="Logo do feminismo"
-        width={160}
-        className="mt-24 m-auto"
-      />
+    <body className="h-full grid-flow-col transition">
+      <div className="w-fit m-auto mt-24 flex">
+        <h1 className="m-auto p-6 text-5xl text-red-600 font-bold">
+          Fala, manas!
+        </h1>
+
+        <Image src={Logo} alt="Logo do feminismo" width={188} />
+      </div>
 
       <div className="text-center mt-20">
         <Link href="/start">
-          <button className="w-48 p-3 text-xl font-semibold bg-black text-white hover:bg-white hover:text-black">
+          <button className="w-52 h-20 p-4 text-3xl font-semibold bg-black text-red-600 rounded-lg hover:bg-red-600 hover:text-black">
             Começar
           </button>
         </Link>
--- src/app/start/page.tsx
+++ src/app/start/page.tsx
@@ -1,15 +1,12 @@
 'use client';
 
 import Image from 'next/image';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination, FreeMode } from 'swiper/modules';
+import Link from 'next/link';
+import { useRef, useState } from 'react';
+import { PanInfo, motion } from 'framer-motion';
+import { FiChevronLeft, FiChevronRight, FiCircle } from 'react-icons/fi';
 
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/free-mode';
-
-import feminismo from '../../../public/feminismo.jpg';
+import feminismo from '../../../public/background.jpg';
 import feminismo2 from '../../../public/feminismo2.jpeg';
 import violencia from '../../../public/violencia.jpg';
 import penha from '../../../public/penha.jpg';
@@ -45,50 +42,118 @@ export default function Start() {
       text: 'A Lei Maria da Penha também teve uma importante vitória em fevereiro de 2012, em decisão do STF, quando foi estabelecido que qualquer pessoa poderia registrar formalmente uma denúncia de violência contra a mulher, e não apenas quem está sob essa violência.',
     },
   ];
+  const constraintsRef = useRef(null);
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const isFirstSlide = currentIndex === 0;
+  const isLastSlide = currentIndex === slides.length - 1;
+
+  const prevSlide = () => {
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+  const nextSlide = () => {
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const goToSlide = (slideIndex: number) => {
+    setCurrentIndex(slideIndex);
+  };
+
+  const swipeSlide = (event: any, info: PanInfo) => {
+    const initialDrag = info.point.x;
+    const finalDrag = info.point.x + info.offset.x;
+
+    if (initialDrag > finalDrag) {
+      nextSlide();
+    } else {
+      prevSlide();
+    }
+  };
 
   return (
-    <div className="w-full p-10 text-white">
-      <Swiper
-        breakpoints={{
-          700: {
-            slidesPerView: 1,
-            spaceBetween: 20,
-          },
-        }}
-        init={false}
-        navigation={true}
-        freeMode={{ enabled: true, sticky: true, minimumVelocity: 4 }}
-        pagination={{ clickable: true }}
-        modules={[Navigation, Pagination, FreeMode]}
+    <motion.div className="p-10 text-white">
+      <motion.div
+        ref={constraintsRef}
+        className="w-fit h-fit p-6 px-10 bg-red-600 duration-500 overflow-hidden"
       >
-        {slides.map((slide, index) => (
-          <SwiperSlide key={slide.title}>
-            <div className="w-fit h-fit p-6 px-10 bg-slate-600 duration-500">
-              <div className="flex">
-                <div className="m-auto px-4 flex">
-                  {slide.image.map((image, index) => (
-                    <div>
-                      <Image
-                        key={image.alt}
-                        src={image.src}
-                        alt={image.alt}
-                        width={image.width}
-                        height={image.height}
-                        className="rounded-2xl px-4"
-                      />
-                    </div>
-                  ))}
-                </div>
-              </div>
+        <motion.div
+          drag="x"
+          dragDirectionLock
+          dragConstraints={{ left: 0, right: 0 }}
+          dragMomentum={false}
+          dragElastic={0.5}
+          dragPropagation={true}
+          onDragStart={swipeSlide}
+          dragTransition={{ bounceStiffness: 800, bounceDamping: 15 }}
+          className="cursor-grab "
+        >
+          <motion.div className="flex">
+            {isFirstSlide ? (
+              <div className="w-8" />
+            ) : (
+              <FiChevronLeft
+                onClick={prevSlide}
+                className="w-8 h-8 mt-[10%] cursor-pointer hover:bg-white hover:text-black rounded-2xl duration-500"
+              />
+            )}
 
-              <h3 className="mt-2 py-4 text-2xl font-semibold">
-                {slide.title}
-              </h3>
-              <p className="text-lg pb-4">{slide.text}</p>
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </div>
+            <motion.div className="m-auto px-4 flex">
+              <motion.div className="grid grid-flow-col gap-x-6">
+                {slides[currentIndex].image.map((images, index) => {
+                  return (
+                    <Image
+                      key={index}
+                      src={images.src}
+                      width={images.width}
+                      height={images.height}
+                      alt={slides[currentIndex].title}
+                      className="rounded-2xl duration-1000"
+                    />
+                  );
+                })}
+              </motion.div>
+            </motion.div>
+
+            {isLastSlide ? (
+              <motion.div className="w-8 h-8 mt-[10%]">
+                <Link href="/">
+                  <button className="text-lg hover:underline">Voltar</button>
+                </Link>
+              </motion.div>
+            ) : (
+              <FiChevronRight
+                onClick={nextSlide}
+                className="w-8 h-8 mt-[10%] cursor-pointer hover:bg-white hover:text-black rounded-2xl duration-500"
+              />
+            )}
+          </motion.div>
+
+          <motion.div className="p-4 flex justify-center">
+            {slides.map((slide, slideIndex) => {
+              return (
+                <motion.div
+                  key={slideIndex}
+                  onClick={() => goToSlide(slideIndex)}
+                  className="px-1 cursor-pointer"
+                >
+                  <FiCircle
+                    className="w-4 h-4"
+                    fill={currentIndex === slideIndex ? '#FFFFFF' : 'none'}
+                  />
+                </motion.div>
+              );
+            })}
+          </motion.div>
+
+          <h3 className="mt-2 py-4 text-2xl font-semibold">
+            {slides[currentIndex].title}
+          </h3>
+          <p className="text-lg">{slides[currentIndex].text}</p>
+        </motion.div>
+      </motion.div>
+    </motion.div>
   );
 }
